66968b61898183c94d541e6525386a0eead734f7
Markus Wallerberger <mwallerb@umich.edu>
2021-09-01 17:52:33 +0200

    More things.

--- test/test_ufunc.py
+++ test/test_ufunc.py
@@ -2,12 +2,12 @@ import numpy as np
 import quadruple
 
 
-def _compare_ufunc(ufunc, x):
+def _compare_ufunc(ufunc, x, ulps=1):
     fx_d = ufunc(x)
     fx_q = ufunc(quadruple.ddarray(x)).hi
 
     # Ensure relative accuracy of 2 ulps
-    np.testing.assert_array_almost_equal_nulp(fx_d, fx_q, 1)
+    np.testing.assert_array_almost_equal_nulp(fx_d, fx_q, ulps)
 
 
 def test_log():
@@ -19,4 +19,7 @@ def test_exp():
     x = np.geomspace(1e-300, 700, 4953)
     x = np.hstack([-x[::-1], 0, x])
     _compare_ufunc(np.exp, x)
-    _compare_ufunc(np.expm1, x)
+
+    # Unfortunately, on Windows expm1 is less precise, so we need to increase
+    # the tolerance slightly
+    _compare_ufunc(np.expm1, x, 2)
