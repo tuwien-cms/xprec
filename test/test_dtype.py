7cb5fa7fad4cb17dfe40a7a99f1746695dc2c284
Markus Wallerberger <markus.wallerberger@tuwien.ac.at>
2021-09-12 17:01:23 +0200

    Check casting.

---
+++ test/test_dtype.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pytest
+
+from xprec import ddouble
+
+
+COMPATIBLE_DTYPES = [
+    np.int8, np.int16, np.int32, np.int64, np.bool_, np.float32, np.float64,
+    np.uint8, np.uint16, np.uint32, np.uint64,
+    ]
+
+
+@pytest.mark.parametrize('other', COMPATIBLE_DTYPES)
+def test_cast_from(other):
+    assert np.can_cast(other, ddouble, 'unsafe')
+    assert np.can_cast(other, ddouble, 'safe')
+
+    x = np.eye(3, dtype=other)
+    y = x.astype(ddouble)
+    assert (x == y).all()
+
+
+@pytest.mark.parametrize('other', COMPATIBLE_DTYPES)
+def test_cast_to(other):
+    assert np.can_cast(ddouble, other, 'unsafe')
+    assert not np.can_cast(ddouble, other, 'safe')
+
+    x = np.eye(3, dtype=ddouble)
+    y = x.astype(other)
+    assert (x == y).all()
+
