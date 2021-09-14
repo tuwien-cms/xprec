21b8355ed6da659e822354fda370acac3e92ce57
Markus Wallerberger <mwallerb@umich.edu>
2021-09-14 13:40:52 +0200

    Stash.

--- pysrc/xprec/__init__.py
+++ pysrc/xprec/__init__.py
@@ -18,7 +18,23 @@ Example:
 """
 __version__ = "0.2.2"
 
+import numpy as _np
+import numpy.core.getlimits as _np_getlimits
+
 from . import _dd_ufunc
 from . import _dd_linalg
 
 ddouble = _dd_ufunc.dtype
+
+
+def _register_finfo():
+    DDouble = ddouble.type
+    key = DDouble(-1).newbyteorder('<').tobytes()
+    limits = _np_getlimits.MachArLike(ddouble,
+                machep=-105, negep=-106, minexp=-1022, maxexp=1024, it=105,
+                iexp=11, ibeta=2, irnd=5, ngrd=0, eps=_dd_ufunc.EPS,
+                epsneg=_dd_ufunc.EPS/2, huge=_dd_ufunc.MAX,
+                tiny=_dd_ufunc.MIN)
+    params = dict(itype=_np.int64, fmt='%s', title="double-double number")
+    _np_getlimits._register_type(limits, key)
+    _np_getlimits._MACHAR_PARAMS[ddouble] = params
